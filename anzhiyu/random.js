f9088fb5cd534d68c835649f7d94406b0e0b7829
effort-xy <qwer1993643@gmail.com>
2024-12-10 13:43:59 +0800

    Site updated: 2024-12-10 13:43:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/10/hello-world/"];function toRandomPost(){
+var posts=["2024/12/10/my/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
